fe11889052bf93fbed4313f1afa97e66f04ecf48
James Vallay <jcv22@pitt.edu>
2022-10-31 18:49:06 -0400

    landing, routing to preferences form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrefFormComponent } from './pref-form/pref-form.component';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent},
+  { path: 'pref-form', component: PrefFormComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingPageComponent,
+    FooterComponent,
+    PrefFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
